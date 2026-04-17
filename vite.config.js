085d5a6c56a2d7ab327b40790d850881861272a5
zzckyy <zackymm2080@gMil.com>
2026-04-17 21:25:01 +0700

    feat: full feature(To-Do, List, About, Settings) - AI Builder(for basic logic build)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import bulma from'bulma'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), bulma()]
+  plugins: [react()]
 })
